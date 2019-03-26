7741e3c8f8c5fc9328cb834c6591a99a71be644c
Luciano Masuero <luchimasuero@gmail.com>
2019-03-25 22:52:58 -0300

    Moment y Datetimepicker

--- src/Resources/Views/Partials/header_css.blade.php
+++ src/Resources/Views/Partials/header_css.blade.php
@@ -1,2 +1,4 @@
+<!-- Bootstrap DateTimePicker -->
+<link href="{{ url('css/bootstrap-datetimepicker.min.css') }}" rel="stylesheet">
 <!-- Include SimpleCMS css -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.7/summernote.css" rel="stylesheet">
--- src/Resources/Views/Partials/header_js.blade.php
+++ src/Resources/Views/Partials/header_js.blade.php
@@ -1,3 +1,7 @@
 <!-- Include SimpleCMS js -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.7/summernote.js"></script>
 <script src="{{ url('js/SimpleCMS.js') }}" type="text/javascript"></script>
+
+{{-- Bootstrap DateTimePicker --}}
+<script src="{{ url('js/bootstrap-datetimepicker.min.js') }}" type="text/javascript"></script>
+<script src="{{ url('js/moment.js') }}" type="text/javascript"></script>
